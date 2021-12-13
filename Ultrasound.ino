8ced7f964696856ba632ed47dd78f66df4bf98fe
yashaswinishah <67494566+yashaswinishah@users.noreply.github.com>
2021-12-13 22:10:32 +0530

    Create Ultrasound.ino

---
+++ Ultrasound.ino
@@ -0,0 +1,31 @@
+int trigpin=9;
+int echopin=8;
+float duration;
+void setup() {
+  
+  pinMode(trigpin,OUTPUT);
+  pinMode(7,OUTPUT);
+  pinMode(echopin,INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  digitalWrite(trigpin,LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigpin,HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigpin,LOW);
+  duration=pulseIn(echopin,HIGH);
+  float cm=duration/29/2;
+  Serial.print("Distance in cm = ");
+  Serial.println(cm);
+  if(cm <= 4.00)
+  {
+    digitalWrite(7,HIGH);
+  }
+  else
+  {
+    digitalWrite(7,LOW);
+  }
+  delay(1000);
+}
